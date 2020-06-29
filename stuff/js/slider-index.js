6af1a94a4b8f7130cd76d67b4dfb316d42b8d272
Mohit Rathore <mrmohitrathoremr@gmail.com>
2020-06-29 10:35:39 +0530

    stuff 2

--- stuff/js/slider-index.js
+++ stuff/js/slider-index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
 	$('#js-main-slider').pogoSlider({
-		autoplay: true,
-		autoplayTimeout: 5000,
+		autoplay: false,
+		autoplayTimeout: 0,
 		displayProgess: true,
 		preserveTargetSize: true,
-		targetWidth: 1000,
+		targetWidth: 100,
 		targetHeight: 300,
 		responsive: true
 	}).data('plugin_pogoSlider');
